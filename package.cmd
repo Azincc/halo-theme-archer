86c2891446ddf830c685a68ff19c74e03db214f7
Azincc <chenglv2000@163.com>
2025-11-01 22:42:25 +0800

    修改next和prev关系

---
+++ package.cmd
@@ -0,0 +1,16 @@
+@echo off
+ECHO 正在准备压缩...
+
+REM 检查旧文件，如果存在则删除 (实现 "覆盖" 效果)
+if exist "theme.zip" (
+    ECHO 发现已存在的 theme.zip，正在删除...
+    del "theme.zip"
+)
+
+ECHO 正在使用 tar 压缩文件...
+REM -a (自动根据扩展名 .zip 选择压缩格式)
+REM -c (创建 Create)
+REM -f (指定文件名 File)
+tar.exe -a -c -f theme.zip templates settings.yaml theme.yaml
+
+ECHO 压缩完成: theme.zip
